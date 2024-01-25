b29b68b9ceace16e28927347a2b8a41dea4a45b0
Chandni <soni.chandni.415@gmail.com>
2024-01-25 17:10:52 +0530

    add user crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+// import { AuthModule } from './auth/auth.module';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'postgres',
+      username: 'postgres',
+      entities: [User], // here we have added user enitity in entities array
+      database: 'pgWithNest',
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+    // AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
